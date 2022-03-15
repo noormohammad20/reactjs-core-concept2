013d0816e106a5d61bc0905de724d606d3165fab
noormohammad20 <mdsaim204@gmail.com>
2022-03-16 01:30:12 +0600

    component

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
+
+  const products = [{ name: 'laptop', price: 56000 },
+  { name: 'phone', price: 56000 },
+  { name: 'watch', price: 34000 },
+  { name: 'tablet', price: 6000 }]
   return (
     <div className="App">
-      <Product></Product>
+      {products.map(product => <Product name={product.name} price={product.price}></Product>)}
+      {/* <Product name="laptop" price="50000"></Product>
+      <Product name="phone" price="75000"></Product>
+      <Product name="watch" price="3500"></Product> */}
     </div>
   )
 }
 
 function Product(props) {
   return (
-    <div>
-      <h3>name:</h3>
-      <p>price:</p>
+    <div className='product'>
+      <h3>name: {props.name}</h3>
+      <p>price:{props.price}</p>
     </div>
 
   )
