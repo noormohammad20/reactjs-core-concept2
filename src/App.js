e2c65ed1b20fdd8b4c0c5e9f56af4a859946252c
noormohammad20 <mdsaim204@gmail.com>
2022-03-16 17:10:57 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,21 +1,79 @@
 import logo from './logo.svg'
 import './App.css'
+import { useEffect, useState } from 'react'
 
 function App() {
 
-  const products = [{ name: 'laptop', price: 56000 },
-  { name: 'phone', price: 56000 },
-  { name: 'watch', price: 34000 },
-  { name: 'tablet', price: 6000 }]
+
   return (
     <div className="App">
-      {products.map(product => <Product name={product.name} price={product.price}></Product>)}
-      {/* <Product name="laptop" price="50000"></Product>
-      <Product name="phone" price="75000"></Product>
-      <Product name="watch" price="3500"></Product> */}
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   )
 }
+
+function ExternalUsers() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{ border: ' 2px solid red', margin: '20px' }}>
+      <h3>name: {props.name}</h3>
+      <p>email: {props.email}</p>
+    </div>
+  )
+}
+
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  const increaseCount = () => setCount(count + 1)
+  const decreaseCount = () => setCount(count - 1)
+
+
+  // const increaseCount = () => {
+  //   const newCount = count + 1
+  //   setCount(newCount)
+  // }
+  return (
+    <div>
+      <h1>count:{count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+
+
+
+// const products = [{ name: 'laptop', price: 56000 },
+//   { name: 'phone', price: 56000 },
+//   { name: 'watch', price: 34000 },
+//   { name: 'tablet', price: 6000 }]
+
+//  {products.map(product => <Product name={product.name} price={product.price}></Product>)}
+// <Product name="laptop" price="50000"></Product>
+// <Product name="phone" price="75000"></Product>
+// <Product name="watch" price="3500"></Product> 
+
 
 function Product(props) {
   return (
